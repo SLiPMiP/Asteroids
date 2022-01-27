b4714c4e04805316c3de14a43188eafa97d25d2e
Anders Lundsby <andersls@mvb.net>
2022-01-27 08:20:20 +0100

    noise gun

--- game/asteroid.js
+++ game/asteroid.js
@@ -46,20 +46,5 @@ function Asteroid(pos, r) {
             return newA
         }
 
-        this.edges = function() {
-            if (this.pos.x > width + this.r) {
-                this.pos.x = -this.r
-            } else if (this.pos.x < -this.r) {
-                this.pos.x = width + this.r
-            }
-            if (this.pos.y > height + this.r) {
-                this.pos.y = -this.r
-            } else if (this.pos.y < -this.r) {
-                this.pos.y = height + this.r
-            }
-        }
-
-
-        pop()
     }
 }

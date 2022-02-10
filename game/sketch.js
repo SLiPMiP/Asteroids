86e74481f4e4ceddda94bd21aebbc4dbf7b4b810
SLiPMiP <slipmip@outlook.dk>
2022-02-10 09:33:52 +0100

    a safasf a dada

--- game/sketch.js
+++ game/sketch.js
@@ -26,12 +26,12 @@ function setup() {
 function draw() {
     background(0)
     for (let i = 0; i < asteroids.length; i++) {
-        if (ship.hits(asteroids[i])) {
-            // noLoop();
-            // fill(230, 230, 230, 50);
-            // textSize(150);
-            // // text('man im dead', 200, 200, 20000, 2000);
-            // text('so bad', 350, 0, 20000, 2000);
+        if (ship.hits(asteroids[i])) {  
+            noLoop();
+            fill(230, 230, 230, 50);
+            textSize(150);
+            text('man im dead', 200, 200, 20000, 2000);
+            text('so bad', 350, 0, 20000, 2000);
         }
         asteroids[i].render()
         asteroids[i].update()
